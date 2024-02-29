b6787ee3daa37335f4449fa7c5dd2a0700cf45c5
Hakan Tekin <hakantekin@Hakan-MacBook-Pro.local>
2024-02-29 14:49:37 +0300

    deniyom hala

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,19 @@ function ingCevir(){
     I'm trying to improve myself in software.
     I am taking Acun Media Academy extended software expertise course
     I create my own projects and try to improve myself as fast as possible.</p>
-    <button class="tekrarTR" onclick="trCevir()">Translate</button>`
-    const tekrarCevir = document.querySelector('.tekrarCevir');
+    <button class="tekrarTR">Çevir</button>`
+}
+
+function tekrarCevir(){
+    const tekrarCevir = document.querySelector('.tekrarTR');
+    tekrarCevir.addEventListener('click', function(e){
+        e.preventDefault();
+        aciklamaKismi.innerHTML = `<div class="aciklamaKismi"><p>Merhaba ben Hakan 19 yaşındayım
+        yazılım konusunda kendimi geliştirmeye çalışıyorum
+        Acun Medya Akademi genişletilmiş yazılım uzmanlığı kursu alıyorum
+        kendi projelerimi oluşturuyorum kendimi olabildiğince hızlı geliştirmeye çalışıyorum.</p></div>`
+    });
+
+
 }
 
