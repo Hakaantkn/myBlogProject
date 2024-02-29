b7d17c3e097a4909f29927612810481374755c14
Hakan Tekin <hakantekin@Hakan-MacBook-Pro.local>
2024-02-29 20:56:09 +0300

    biti

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,10 +19,10 @@ function ingCevir(){
 function tekrar(tekrarCevir){
 
     tekrarCevir.addEventListener('click', function(){
-        aciklamaKismi.innerHTML = `<div class="aciklamaKismi"><p>Merhaba ben Hakan 19 yaşındayım
+        aciklamaKismi.innerHTML = `<p>Merhaba ben Hakan 19 yaşındayım
         yazılım konusunda kendimi geliştirmeye çalışıyorum
         Acun Medya Akademi genişletilmiş yazılım uzmanlığı kursu alıyorum
-        kendi projelerimi oluşturuyorum kendimi geliştirmeye çalışıyorum.</p><button class="translateBtn" onclick="ingCevir()">translate</button></div>`
+        kendi projelerimi oluşturuyorum kendimi geliştirmeye çalışıyorum.</p><button class="translateBtn" onclick="ingCevir()">translate</button>`
 
     });
 
